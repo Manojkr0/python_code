ba091f297557af801db9ca338fb5530ec8836d3d
manoj kumar <venkatamanojkumar0510@gmail.com>
2025-05-31 18:04:53 +0530

    ECR

---
+++ app/app.py
@@ -0,0 +1,11 @@
+
+import json
+
+def hello(event, context):
+    return {
+        'statusCode': 200,
+        'body': json.dumps({
+            'message': 'Hello from Lambda!',
+            'version': '1.0'
+        })
+    }
